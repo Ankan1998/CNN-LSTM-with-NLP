9389798aa744f32bd2a4d698dd785f8d13ba7926
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-22 21:39:21 +0530

    torchtext custom field

---
+++ dataset_processing/custom_field.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from torchtext.legacy.data import Field
+from text_preprocessing import tweet_cleaning
+from text_preprocessing import tokenizer_en
+
+def field_maker(max_doc_len):
+    Text = Field(
+        preprocessing=tweet_cleaning.tweet_preprocessing,
+        sequential=True,
+        tokenize=tokenizer_en.tokenize,
+        batch_first=True,
+        include_lengths=True,
+        fix_length=max_doc_len,
+        lower=True
+    )
+    Label = Field(
+        sequential=False,
+        use_vocab=False,
+        pad_token=None,
+        unk_token=None
+    )
+
+    fields = [('text', Text), ('labels', Label)]
+    return fields
+
+if __name__=="__main__":
+    csv_file = r'C:\Users\Ankan\Downloads\train_tweet.csv'
+    df = pd.read_csv(csv_file)
