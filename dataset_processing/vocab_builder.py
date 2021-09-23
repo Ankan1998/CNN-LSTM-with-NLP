46340d79c34c5750ac114154185d26b712b2df15
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-23 12:49:45 +0530

    vocab builder correction

--- dataset_processing/vocab_builder.py
+++ dataset_processing/vocab_builder.py
@@ -1,10 +1,15 @@
 from dataset_processing.custom_field import field_maker
+from dataset_processing.train_test_data_maker import train_test_maker, train_val_splitter
 
 
-def vocab_builder(train_data,val_data):
-    field = field_maker()
-    text = field['tweet'][1]
-    text.build_vocab(train_data,val_data, max_size=10000,min_freq=2)
-    return text.vocab
+def vocab_builder(train_data):
+    train_data.fields['text'].build_vocab(train_data, max_size=10000,min_freq=2)
+    return train_data.fields['text'].vocab
 
+if __name__=="__main__":
 
+    train_file = r'C:\Users\Ankan\Downloads\train_tweet.csv'
+    test_file = r'C:\Users\Ankan\Downloads\test_tweets.csv'
+    trn_data, tst_data = train_test_maker(train_file,test_file)
+    txt_vocab = vocab_builder(trn_data)
+    print(txt_vocab.stoi)
