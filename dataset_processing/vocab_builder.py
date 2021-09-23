ad7874380b8c4d6216217c6675be00de4a682cac
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-23 11:46:44 +0530

    vocab builder added

---
+++ dataset_processing/vocab_builder.py
@@ -0,0 +1,10 @@
+from dataset_processing.custom_field import field_maker
+
+
+def vocab_builder(train_data,val_data):
+    field = field_maker()
+    text = field['tweet'][1]
+    text.build_vocab(train_data,val_data, max_size=1000)
+    return text.vocab
+
+
