5a73bf80136644eba535add62c1b82577cc5b128
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-28 15:37:05 +0530

    vocab builder arguments updated

--- dataset_processing/vocab_builder.py
+++ dataset_processing/vocab_builder.py
@@ -1,9 +1,8 @@
-from dataset_processing.custom_field import field_maker
-from dataset_processing.train_test_data_maker import train_test_maker, train_val_splitter
+from dataset_processing.train_test_data_maker import train_test_maker
 
 
-def vocab_builder(train_data):
-    train_data.fields['text'].build_vocab(train_data, max_size=100,min_freq=2)
+def vocab_builder(train_data,max_vocab, min_freq):
+    train_data.fields['text'].build_vocab(train_data, max_size=max_vocab,min_freq=min_freq)
     train_data.fields['labels'].build_vocab(train_data)
     return train_data
 
@@ -12,5 +11,5 @@ if __name__=="__main__":
     train_file = r'C:\Users\Ankan\Downloads\sub_train.csv'
     test_file = r'C:\Users\Ankan\Downloads\sub_test.csv'
     trn_data, tst_data = train_test_maker(train_file,test_file)
-    txt_vocab,lbl_vocab = vocab_builder(trn_data)
+    txt_vocab,lbl_vocab = vocab_builder(trn_data,1000,2)
     print(txt_vocab.stoi)
