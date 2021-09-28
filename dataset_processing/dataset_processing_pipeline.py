487f7a381d4d0e0ab8ec041809d841a092e81000
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-28 15:40:58 +0530

    data processing pipeline added

--- dataset_processing/dataset_processing_pipeline.py
+++ dataset_processing/dataset_processing_pipeline.py
@@ -2,27 +2,27 @@ from dataset_processing.train_test_data_maker import train_test_maker, train_val
 from dataset_processing.vocab_builder import vocab_builder
 from dataset_processing.data_iterator import dataset_itr
 
-def data_pipeline(train_file, test_file,batch_size=128):
+def data_pipeline(train_file, test_file,max_vocab=1000,min_freq=2,batch_size=128):
 
     # Cleaned Data
     trn_data, test_data = train_test_maker(train_file,test_file)
 
     # Build Vocab
-    train_data = vocab_builder(trn_data)
-
+    train_data = vocab_builder(trn_data,max_vocab,min_freq)
+    vocab_size = len(train_data.fields['text'].vocab)
     # Train and Val Split
     train_data, val_data = train_val_splitter(train_data)
 
     # Iterator
     train_itr, val_itr, test_itr = dataset_itr(train_data, val_data, test_data,batch_size)
 
-    return train_itr, val_itr, test_itr
+    return train_itr, val_itr, test_itr, vocab_size
 
 if __name__=="__main__":
 
     train_file = r'C:\Users\Ankan\Downloads\sub_train.csv'
     test_file = r'C:\Users\Ankan\Downloads\sub_test.csv'
-    train_itr, val_itr, test_itr = data_pipeline(train_file,test_file,2)
+    train_itr, val_itr, test_itr,vocab_size = data_pipeline(train_file,test_file,2)
     for batch in train_itr:
         print(batch.text)
         # print(batch.labels)
