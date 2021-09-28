55725da4d205eedd382ef4bc0bdc4882f8c6fc24
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-28 15:41:11 +0530

    bug fixed

--- dataset_processing/data_iterator.py
+++ dataset_processing/data_iterator.py
@@ -3,7 +3,8 @@ def dataset_itr(train_data,val_data,test_data, BATCH_SIZE=128,device='cpu'):
     train_itr, val_itr, test_itr = BucketIterator.splits(
         (train_data,val_data,test_data),
         batch_size= BATCH_SIZE,
-        device = device
+        device = device,
+        sort=False
     )
 
     return train_itr,val_itr,test_itr
