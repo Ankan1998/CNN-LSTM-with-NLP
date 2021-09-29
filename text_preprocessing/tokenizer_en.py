e1387f398db4d708fda111f75783934c229acdd6
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-29 19:44:39 +0530

    single tweet processing added

--- text_preprocessing/tokenizer_en.py
+++ text_preprocessing/tokenizer_en.py
@@ -1,6 +1,6 @@
 import spacy
 import pandas as pd
-from text_preprocessing.tweet_cleaning import tweet_preprocessing_series
+from text_preprocessing.tweet_cleaning import tweet_preprocessing_series,single_tweet_preprocessing
 import subprocess
 
 
@@ -16,6 +16,8 @@ def tokenize(sentence):
 if __name__=="__main__":
     csv_file = r'C:\Users\Ankan\Downloads\train_tweet.csv'
     df = pd.read_csv(csv_file)
-    df['cleaned'] = tweet_preprocessing_series(df['tweet'])
-    print(df['cleaned'][0])
-    print(tokenize(df['cleaned'][0]))
+    # df['cleaned'] = tweet_preprocessing_series(df['tweet'])
+    # print(df['cleaned'][0])
+    sentence = single_tweet_preprocessing(df['tweet'][0])
+    print(sentence)
+    print(tokenize(sentence))
--- text_preprocessing/tweet_cleaning.py
+++ text_preprocessing/tweet_cleaning.py
@@ -17,9 +17,15 @@ def tweet_preprocessing_series(tweet_series):
         cleaned_list.append(reg_clean)
     return pd.Series(cleaned_list)
 
+def single_tweet_preprocessing(tweet):
+    clean_sentence = pr.clean(tweet)
+    reg_sentence_clean = cleanup_text(clean_sentence)
+    return reg_sentence_clean
+
 if __name__=="__main__":
     csv_file = r'C:\Users\Ankan\Downloads\train_tweet.csv'
     df = pd.read_csv(csv_file)
-    df['cleaned'] = tweet_preprocessing_series(df['tweet'])
+    # df['cleaned'] = tweet_preprocessing_series(df['tweet'])
     print(df['tweet'][0])
-    print(df['cleaned'][0])
+    print(single_tweet_preprocessing(df['tweet'][0]))
+    # print(df['cleaned'][0])
